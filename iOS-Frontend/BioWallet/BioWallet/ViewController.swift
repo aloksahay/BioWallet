86037fc99d018e48eea3f453cae67a2aa4f8231b
Alok Sahay <alok.sahay87@gmail.com>
2023-04-15 14:31:26 +0200

    Project creation

---
+++ iOS-Frontend/BioWallet/BioWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BioWallet
+//
+//  Created by Alok Sahay on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
